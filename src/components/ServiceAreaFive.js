0d60a5fbbf319becb428db7f513eed8982241eab
amitbd591 <122339814+amitbd591@users.noreply.github.com>
2023-03-11 11:24:46 +0600

    ServiceAreaFive add

---
+++ src/components/ServiceAreaFive.js
@@ -0,0 +1,136 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const ServiceAreaFive = () => {
+  return (
+    <>
+      {/*=================== service area five start ===================*/}
+      <div className='service-area bg-gray bg-relative pd-top-120 pd-bottom-120'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <div className='col-lg-6'>
+              <div className='section-title text-center'>
+                <h6 className='sub-title'>Our Best Service</h6>
+                <h2 className='title'>
+                  Unlocking The Potential Of Data With Ai
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className='row custom-no-gutter'>
+            <div className='col-lg-4 col-md-6'>
+              <div className='single-service-inner-3 single-service-inner-3-left'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/7.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>AI Consult</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+              <div className='single-service-inner-3 single-service-inner-3-left'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/8.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>Data Mind</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+              <div className='single-service-inner-3 single-service-inner-3-left mb-0'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/9.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>IntelliSense</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6 bg-blue-right d-lg-inline-block d-none'>
+              <div className='service-thumb service-middle-section align-self-end'>
+                <img src='assets/img/about/22.png' alt='img' />
+              </div>
+            </div>
+            <div className='col-lg-4 col-md-6'>
+              <div className='single-service-inner-3 single-service-inner-3-right'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/10.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>Neural Solutions</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+              <div className='single-service-inner-3 single-service-inner-3-right'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/11.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>Wise Logic</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+              <div className='single-service-inner-3 single-service-inner-3-right mb-0'>
+                <div className='thumb'>
+                  <div className='thumb-inner'>
+                    <img src='assets/img/service-icon/12.svg' alt='img' />
+                  </div>
+                </div>
+                <div className='details'>
+                  <h5 className='mb-3'>
+                    <a href='service-details.html'>Smart Vision</a>
+                  </h5>
+                  <p className='mb-0'>
+                    Aliquam eros justo, posuere loborti inh thi viverra coin
+                    design here laoreet min ouimatti posuere lorem
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* =================== service area end ===================*/}
+    </>
+  );
+};
+
+export default ServiceAreaFive;
--- src/pages/HomeFive.js
+++ src/pages/HomeFive.js
@@ -2,6 +2,7 @@ import React from "react";
 import AboutAreaFive from "../components/AboutAreaFive";
 import BannerFive from "../components/BannerFive";
 import NavbarFour from "../components/NavbarFour";
+import ServiceAreaFive from "../components/ServiceAreaFive";
 
 const HomeFive = () => {
   return (
@@ -14,6 +15,9 @@ const HomeFive = () => {
 
       {/* About Area Five */}
       <AboutAreaFive />
+
+      {/* Service Area Five */}
+      <ServiceAreaFive />
     </>
   );
 };
